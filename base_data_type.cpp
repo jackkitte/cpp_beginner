e37542dccd19af14dcd9206f7677770edd23ccc9
jackkitte <jackkitte735@gmail.com>
2020-08-09 18:05:00 +0900

    データ型の続き

--- base_data_type.cpp
+++ base_data_type.cpp
@@ -15,4 +15,22 @@ int main() {
     cout << "u_int1 : " << u_int1 << endl;
     cout << "long1 : " << long1 << endl;
     cout << "u_long1 : " << u_long1 << endl;
+
+    double double1 = 10.1;
+    float float1 = 12.22f;
+
+    cout << "double1 : " << double1 << endl;
+    cout << "float1 : " << float1 << endl;
+
+    char char1 = 'a';
+
+    cout << "char1 : " << char1 << endl;
+
+    bool bool1 = true;
+    bool bool2 = false;
+
+    cout << "bool1 : " << bool1 << endl;
+    cout << "bool2 : " << bool2 << endl;
+
+    return 0;
 }
