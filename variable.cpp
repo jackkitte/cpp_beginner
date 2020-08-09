b76c65df72eced0ec6ac42878fbe2320ae32a24a
jackkitte <jackkitte735@gmail.com>
2020-08-09 17:55:12 +0900

    変数

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int price = 100;
+    cout << "price : " << price << endl;
+
+    price = 150;
+    cout << "price : " << price << endl;
+
+    return 0;
+}
